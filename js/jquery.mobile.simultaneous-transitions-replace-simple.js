85d33b0fac70da87ac320b7e1cbbb9bd654767e2
Ara <info@app4brides.com>
2012-12-17 07:40:09 +0100

    trace

---
+++ js/jquery.mobile.simultaneous-transitions-replace-simple.js
@@ -0,0 +1,32 @@
+﻿$(document).bind("mobileinit", function installSimultaneousTransitions() {
+
+  var simultaneousHandler = $.mobile.transitionHandlers.simple,
+
+  classicTransitionKeys = [
+    "none",
+    "slide",
+    "slideup",
+    "slidedown",
+    "fade",
+    "flip",
+    "pop"
+    ];
+
+  plusTransitionKeys = [
+    "slideoverleft",
+    "slideoverright",
+    "slidepushup",
+    "slidepushdown"
+    ];
+
+  $.each(classicTransitionKeys, function(i) {
+    $.mobile.transitionHandlers[classicTransitionKeys[i]] = simultaneousHandler;
+    });
+
+  $.each(plusTransitionKeys, function(i) {
+    var key = plusTransitionKeys[i];
+    $.mobile.transitionHandlers[key] = simultaneousHandler;
+    $.mobile.transitionFallbacks[key] = "fade";
+    });
+
+});
---
+++ js/jquery.mobile.transition-handler-simple.js
@@ -0,0 +1,49 @@
+﻿$(document).on("mobileinit", function createSimpleHandler() {
+  var simpleHandler = function( name, reverse, $to_p, $from_p ) {
+
+    var deferred = new $.Deferred(),
+        none = !$.support.cssTransitions || !name || name === "none",
+        $to = $to_p,
+        $from = $from_p,
+
+    toggleViewportClass = function() {
+      $.mobile.pageContainer.toggleClass( "ui-mobile-viewport-transitioning viewport-" + name );
+      },
+
+    done = function() {
+      if ( $from ) {
+        $from
+          .height( "" )
+          .removeClass( $.mobile.activePageClass + " out in reverse " + name );
+        }
+      $to
+        .height( "" )
+        .removeClass( "out in reverse " + name );
+      toggleViewportClass();
+      deferred.resolve( name, reverse, $to, $from, true );
+      };
+
+    $to.addClass( $.mobile.activePageClass );
+    // Note: In many applications the following line may be unnecessary, and may be removed
+    $.mobile.focusPage( $to );  // Send focus to page as it is now display: block
+    if (none) {
+      if ( $from ) {
+        $from.removeClass( $.mobile.activePageClass );
+        }
+      deferred.resolve( name, reverse, $to, $from, true );
+      } else {
+        var screenHeight = $.mobile.getScreenHeight(),
+            reverseClass = reverse ? " reverse" : "";
+        toggleViewportClass();
+        $to.animationComplete( done );
+        $to
+          .height( screenHeight ) // Note: setting an explicit height helps eliminate tiling in the transitions
+          .addClass( name + " in" + reverseClass );
+         $from
+           .height( screenHeight )
+           .addClass( name + " out" + reverseClass );
+      }
+    return deferred.promise();
+    }
+  $.mobile.transitionHandlers.simple = simpleHandler;
+});
